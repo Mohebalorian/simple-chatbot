5c3486e2429b18ceb18f0a42c3aaaa8ad6bd2955
Mohebalorian <s.m.zia408@gmail.com>
2026-09-18 12:29:54 +0500

    Refactor chatbot responses and improve structure

--- chatbot.c
+++ chatbot.c
@@ -1,42 +1,48 @@
 #include <stdio.h>
 #include <string.h>
 
+int main() {
+    char message[200];
 
-int main(void) {
-
-    char message [200];
-    
-    printf("Bot: Hi I am a chatbot. How can I help you?\n");
+    printf("Bot: Hi I am a chatbot! How can I help you?\n");
     printf("You: ");
-     
 
- while(1) {
-      
-   if(fgets(message, 200, stdin)==NULL{
-       break;
-   }
-
-     if(strstr(message, "bye") != NULL) {
-       printf("Bot: Goodbye! Have a great day!\n");
-       break;
-    }
-
-   else if(strstr(message, "hello") != NULL) {
-       printf("Bot: Hello! How are you?\n");
-    }
-       else if(strstr(message, "how are you") != NULL) {
-              printf("Bot: I am good, thank you for asking!\n");
+    while(1) {
+         if(fgets(message,200,stdin)==NULL) {
+            break;
+         }
+         if(strstr(message,"bye")!=NULL){
+            printf("Bot: Bye have a great day!\n");
+            break;
+         }
+         else{
+            if(strstr(message,"hi")!=NULL || strstr(message,"hello")!=NULL){
+            printf("Bot: Hi how are you doing?\n");
+            printf("You:");
             }
-              else if(strstr(message, "what is your name") != NULL) {
-                     printf("Bot: I am a simple chatbot created to assist you.\n");
+            else{
+                if(strstr(message,"fine")!=NULL || strstr(message,"good")!=NULL || strstr(message,"great")!=NULL){
+                printf("Bot: Well I am not. Tell me how can I help you\n");
+                printf("You:");
+                }
+                else{
+                    if(strstr(message,"bad")!=NULL || strstr(message,"terrible")!=NULL){
+                    printf("Bot: Twinning fr. Now how can I help you\n");
+                    printf("You:");
                     }
-                     else {
-                            printf("Bot: I'm sorry, I don't understand that.\n");
-                     }
-    
- }
- return 0;  
+                     else{
+                         printf("Bot: Sorry I do not understand\n");
+                         printf("You:");
+                        }
+                    }
+                }
+         
+         
+         }
+         
+     
+     }     
+        
+   return 0;
+
 }
-
-
-    
